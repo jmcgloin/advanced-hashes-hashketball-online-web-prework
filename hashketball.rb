63a80faaaa022ed7e868f65dd6bd58ecc9247792
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 01:37:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,16 +1,28 @@
 def game_hash()
   :home => {
-    :team_name => "Brooklyn Nets"
-    :colors => ["Black", "White"]
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
     :players => {
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
+      "Alan Anderson" => {
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      },
+      "Reggie Evans" => {
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      }
     }
   }
   :away => {
