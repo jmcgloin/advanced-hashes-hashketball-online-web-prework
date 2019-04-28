7ca8d8eb59c19ef8d18b0e68e51db5cc8d692a97
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 01:38:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,6 +14,16 @@ def game_hash()
         :slam_dunks => 1
       },
       "Reggie Evans" => {
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      },
+      "Brook Lopez" => {
         :number =>
         :shoe =>
         :points =>
